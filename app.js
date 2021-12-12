aae89f2361a6d708be1494de0aebac76e9b97e4c
OrishabaFamious <fameousmj@gmail.com>
2021-12-12 14:02:18 +0300

    added gamelevels

---
+++ app.js
@@ -0,0 +1,20 @@
+const rElement = document.getElementById("r");
+const gElement = document.getElementById("g");
+const bElement = document.getElementById("b");
+
+const levels = Array.from(document.getElementsByClassName("mode"));
+
+let gamelevel = levels.find((level) => {
+    const classList = Array.from(level.classList);
+    return classList.includes("selected");
+}).innerHTML;
+
+
+levels.forEach(level =>{
+    level.addEventListener("click", function(){
+        levels.forEach(mode => mode.classList.remove("selected"));
+        this.classList.add("selected");
+
+        gamelevel = this.innerHTML;
+    });
+}); 
--- scripts/app.js
+++
@@ -1,5 +0,0 @@
-const rElement = document.getElementById("r");
-const gElement = document.getElementById("g");
-const bElement = document.getElementById("b");
-
-console.log(rElement)
